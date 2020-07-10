a0deada6b2620b8474575b8a29b917968da580d6
mariem86 <66726955+mariem86@users.noreply.github.com>
2020-07-10 15:56:50 +0200

    Add files via upload

---
+++ INDEX.js
@@ -0,0 +1,35 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  getAge = function (pet)
+  {
+  return new Date().getFullYear() - pet.bornOn;
+  return new Date().getFullYear() - pet.bornOn;
+  }
+  
+  const petsWithAge = [];
+age.ForEach(getAge);
+  const pet = pets[i];
+  pet.age = getAge(pet);
+
+  petsWithAge.push(pet);
+
+console.log(petsWithAge);
+  
+const dogs = [];
+type.ForEach( getAge)
+ const type  = pets[i];
+  if (pet.type === "dog") {
+    dogs.push(pet);
+  }
+console.log(dogs);
+let jasper;
+name.ForEach( getAge)
+  const name = pets[i];
+  if (pet.name === "Jasper") {
+    jasper = pet;
+  }
+console.log(`Jasper is ${jasper.age} years old`);
